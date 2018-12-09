419d7f792f8df0a931402291ef5f135e93069ada
WangZhenyu <847315251@qq.com>
2018-12-09 19:36:13 +0800

    更新

--- JavaEE Framework/spring-cloud-security/app-feign/src/main/java/top/zywork/controller/TestController.java
+++ JavaEE Framework/spring-cloud-security/app-feign/src/main/java/top/zywork/controller/TestController.java
@@ -1,11 +1,9 @@
 package top.zywork.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import top.zywork.client.TestClient;
 import top.zywork.vo.User;
 
