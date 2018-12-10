31e090c866493443b5a944c043fbfb2f1c90c372
WangZhenyu <847315251@qq.com>
2018-12-10 11:45:51 +0800

    增加了zuul熔断器与超时配置，超时配置中包含有ribbon和hystrix

---
+++ JavaEE Framework/spring-cloud-security/app-zuul/src/main/java/top/zywork/bean/ResponseStatusVO.java
@@ -0,0 +1,41 @@
+package top.zywork.bean;
+
+public class ResponseStatusVO {
+
+    private Integer code;
+    private String message;
+    private Object data;
+
+    public ResponseStatusVO() {
+    }
+
+    public ResponseStatusVO(Integer code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
---
+++ JavaEE Framework/spring-cloud-security/app-zuul/src/main/java/top/zywork/zuul/TestServiceFallbackProvider.java
@@ -0,0 +1,62 @@
+package top.zywork.zuul;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import top.zywork.bean.ResponseStatusVO;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class TestServiceFallbackProvider implements FallbackProvider {
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                ResponseStatusVO statusVO = new ResponseStatusVO(10002, "服务不可用", null);
+                ObjectMapper objectMapper = new ObjectMapper();
+                return new ByteArrayInputStream(objectMapper.writeValueAsBytes(statusVO));
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
